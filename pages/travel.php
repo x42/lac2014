6fdc4dd66c59c12651af22fc33591a81207bafe5
Robin Gareus <robin@gareus.org>
2013-10-17 01:31:54 +0200

    update page content...

--- pages/travel.php
+++ pages/travel.php
@@ -1,29 +1,29 @@
-<h1>Location</h1>
+<p class="red">Information on this page is work in progres, please stay tuned.</p>
 
 <h1>Traveling to Karlsruhe</h1>
 
-<h3>From the airport</h3>
-
 <p class="red"> Don't forget to check right now the validity of your passport and any visa requirements you might have to enter the European Union</p>
 
+<h3>By Air</h3>
+
 <p>
-Frankfurt airport + train ?  TODO.
+TODO: Frankfurt Airport + train = easy :)
 </p>
 
-<h2>By Train</h2>
+<h3>By Train</h3>
 
 <p>
 Please see the <a href="http://reiseauskunft.bahn.de/" rel="external">Deutsche Bahn travel information service</a>.
 </p>
 
 
-<h2>By Car</h2>
+<h3>By Car</h3>
 <p>
-    From the North: Highway A5, exit &laquo;KA-Mitte&raquo;,<br/>
+    From north: Highway A5, exit &laquo;KA-Mitte&raquo;,<br/>
     take the &laquo;S&uuml;dtangente&raquo; direction Landau,<br/>
     follow the &laquo;ZKM&raquo; sign.<br/>
 <br/>
-    From the South: Highway A5, exit &laquo;KA-S&uuml;d&raquo;,<br/>
+    From south: Highway A5, exit &laquo;KA-S&uuml;d&raquo;,<br/>
     follow the &laquo;ZKM&raquo; sign.<br/>
 <br/>
     Parking space for 700 cars available below ZKM,<br/>
@@ -32,9 +32,9 @@ Please see the <a href="http://reiseauskunft.bahn.de/" rel="external">Deutsche B
 
 <div class="center"><img src="img/zkm_anfahrt.png" alt="car directions" /></div>
 
-<h2>By tramway</h2>
+<h3>By Tramway</h3>
 <p class="red">
-Due to construction work in Karlsruhe we kindly ask you to check the current situation for trams and busses on the website KVV Karlsruher Verkehrsverbund.
+Due to construction work in Karlsruhe we kindly ask you to check the current situation for trams and busses on the website KVV Karlsruher Verkehrsverbund (see below).
 </p>
 <p>
 From the train station, take line 2E<br/>
@@ -43,7 +43,7 @@ direction &laquo;Siemensallee&raquo;, get off at &laquo;ZKM&raquo;<br/>
 </p>
 
 
-<h2>By Bus</h2>
+<h3>By Bus</h3>
 <p>
 From the train station, take bus number 55<br/>
 direction &laquo;ZKM/K&uuml;hler Krug&raquo;, get off at &laquo;Lorenzstra&szlig;e&raquo;<br/>
@@ -52,22 +52,32 @@ direction &laquo;ZKM/K&uuml;hler Krug&raquo;, get off at &laquo;Lorenzstra&szlig
 
 <h1>Getting around in Karlsruhe</h1>
 <p>
-The best way to get around in Karlsruhe is by ..
+The best way to get around in Karlsruhe is by .. (TODO)
 </p>
 
-<h2>Useful Links</h2>
+<h3>Useful Links</h3>
 <ul>
   <li><a href="http://stadtplan.karlsruhe.de/" rel="external">City Map Karlsruhe</a></li>
   <li>Local Public Transport: <a href="http://http//www.kvv.de" rel="external">KVV Karlsruher Verkehrsverbund</a></li>
   <li><a href="http://www.taxi-zentrale-karlsruhe.de/" rel="external">Taxi-Service Karlsruhe</a></li>
 </ul>
 
+<div style="height:4em;"></div>
 
 <h1>Staying in Karlsruhe</h1>
 
 <h3>Hotels</h3>
 
-<h3>Youth hostels</h3>
+<p>
+TODO. for now, <a href="http://lmgtfy.com/?q=hotels+near+zkm+karlsruhe" rel="external">see here</a>.
+</p>
+
+<h3>Hostels</h3>
 <p>
 For those who prefer a cheap option,...
 </p>
+
+<ul>
+  <li><a href="http://www.aohostels.com/en/karlsruhe/karlsruhe-hauptbahnhof/" rel="external">A&O</a></li>
+  <li><a href="http://karlsruhe.jugendherberge-bw.de/" rel="external">Youth Hostel - Jugendherberge Karlsruhe</a></li>
+</ul>
