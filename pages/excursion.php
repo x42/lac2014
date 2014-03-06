13ba9c0d5213c78751f86b3a87cddf24c1134b5b
Robin Gareus <robin@gareus.org>
2014-03-06 01:04:57 +0100

    stub for excursion

---
+++ pages/excursion.php
@@ -0,0 +1,31 @@
+<h1>Excursion</h1>                                                                                                                             
+
+<p>
+<em>Will you still be around Sunday afternoon?</em>
+<br/>
+The final event of the conference will be a trip to the countryside to a small village in the &laquo;Kranichgau&raquo;, a hilly region in between the Black Forest and Odenwald about 40 mins east of Karlsruhe.
+<br/>
+We'll visit the <a href="https://en.wikipedia.org/wiki/Maulbronn_Monastery">Kloster Maulbronn</a> which is the best-preserved medieval Cistercian monastery complex in Europe. Apart from its achitectural diversity (Romanesque art to late Gothic) it features a unique acoustic, not to mention a brewery with refreshing Kloster-bier.
+</p>
+
+<div class="ptitle">
+<a href="https://commons.wikimedia.org/wiki/File:Kloster_Maulbronn_2009.jpg" rel="external"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Kloster_Maulbronn_2009.jpg/800px-Kloster_Maulbronn_2009.jpg" alt="Kloster Maulbronn"/></a>
+</div>
+
+<h2>Practical Information</h2>
+
+<h3>When and how long?</h3>
+<p>
+We will leave at 11am in front of the ZKM just outside the main venue with two busses.<br/>
+The return is scheduled around 5pm.
+</p>
+
+<h3>How much is the trip?</h3>
+<p>
+The bus trip is sponsored by LAC, but the drinks are on you :)
+</p>
+
+<h3>Where do I sign up?</h3>
+<p>
+At the registration desk at the conference venue.
+</p>
