21c263bba6d0045744c0e7efa530c77ac9581545
Robin Gareus <robin@gareus.org>
2014-03-28 12:42:21 +0100

    fix HTML and cleanup excursion info

--- pages/excursion.php
+++ pages/excursion.php
@@ -8,26 +8,27 @@ The final event of the conference will be a trip to to &laquo;<a rel="external" 
 </p>
 
 <ul>
-<li>At <strong>12:00</strong> we will visit the Museum For Music Automata: <a rel="external" href="http://www.dmm-bruchsal.de/">Deutsches Musikautomten-Museum</a> located in the Château of Bruchsal (Visiting the museum is voluntary, and you can stay as long as you wish until the museum closes at 17h).
+<li>At <strong>12:00</strong> we will visit the Museum For Music Automata: <a rel="external" href="http://www.dmm-bruchsal.de/">Deutsches Musikautomten-Museum</a> located in the Château of Bruchsal. (Visiting the museum is voluntary, and you can stay as long as you wish until the museum closes at 17h.)
 <br/>
-The Museum houses the most important collection of music machines worldwide. The around 500 exhibits of the museum include developments from early handicraft production of the 17th and 18th Century until its golden age in the industrial age of the late 19th Century and finally to the demise of the mechanical automata during the time of the Great Depression in the late twenties in the 20th Century.</li>
+<em>
+The Museum houses the most important collection of music machines worldwide. The around 500 exhibits of the museum include developments from early handicraft production of the 17th and 18th Century until its golden age in the industrial age of the late 19th Century and finally to the demise of the mechanical automata during the time of the Great Depression in the late twenties in the 20th Century.</em></li>
 <li>Around <strong>15:00</strong> we'll meet in a tavern close-by (<a href="http://www.brauhaus-wallhall-bruchsal.de/">Brauhaus Wallhall</a> - to be confirmed) to close the conference in style. Participation is voluntary but highly recommended :)</li>
 </ul>
 
 <p>&nbsp;</p>
 
 <div class="ptitle">
-<a href="https://en.wikipedia.org/wiki/File:BruchsalSchlossHofPano2.jpg" rel="external"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/BruchsalSchlossHofPano2.jpg/1024px-BruchsalSchlossHofPano2.jpg" alt="Schloss Bruchsal" style="width:90%; margin:0 auto;"/></a><br>
+<a href="https://en.wikipedia.org/wiki/File:BruchsalSchlossHofPano2.jpg" rel="external"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/BruchsalSchlossHofPano2.jpg/1024px-BruchsalSchlossHofPano2.jpg" alt="Schloss Bruchsal" style="width:90%; margin:0 auto;"/></a><br/>
 <small>Château of Bruchsal</small>
 </div>
 
 <h2>Practical Information</h2>
 
 <h3>When and how long?</h3>
-<p>
-We will leave at <strong>11:00</strong> in front of the ZKM just outside the main venue.<br/>
-We will leave Bruchsal at 18h and the scheduled arrival back in Karlsruhe is around <strong>19:00</strong>.
-</p>
+<ul>
+<li>We will leave at <strong>11:00</strong> in front of the ZKM just outside the main venue.</li>
+<li>We will leave Bruchsal at 18h and the scheduled arrival back in Karlsruhe is around <strong>19:00</strong>.</li>
+</ul>
 
 <h3>How much is the trip?</h3>
 <p>
